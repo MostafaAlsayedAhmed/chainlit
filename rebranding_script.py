35b1468eaca963cd5dc6cabb4dff20b8a15f22d3
MostafaAlsayedAhmed <mostafakhairsayed@gmail.com>
2025-02-12 15:57:42 +0200

    re-add public folder

---
+++ rebranding_script.py
@@ -0,0 +1,17 @@
+"""
+# Colors:
+  /public/theme.json
+
+# Logo:
+  /public/logo_dark.png & /public/logo_light.png  & /public/favicon.png  (Note: public folder out of the frontend folder)
+
+
+# Themeing - Custom Styles & Scripts:
+  You can inject them into the application by adding the file (in public folder) [or via an external link for css] to the config.toml under [UI] configrations
+
+# Localization (adding ar.json)
+
+# Footer
+
+# Icons
+"""
